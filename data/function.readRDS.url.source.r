17b04c9e2753c3fb4176fc100112d697034d2a2c
mkim0710 <mkim0710@gmail.com>
2019-08-10 11:18:37 +0900

    Create function.readRDS.url.source.r

---
+++ data/function.readRDS.url.source.r
@@ -0,0 +1,4 @@
+nhefs = readRDS(gzcon(url("https://github.com/mkim0710/causal-inference-book/raw/master/data/nhefs.rds")))
+nhefs = readRDS(url("https://github.com/mkim0710/causal-inference-book/raw/master/data/nhefs.rds"))
+
+nhefs.surv = readRDS(url("https://github.com/mkim0710/causal-inference-book/raw/master/data/nhefs.surv.rds"))
